7f6341fce9c5aef085ab2f3e59063921816d9a2d
JohnThirdt <lvantretiyakov2018@gmail.com>
2024-04-20 15:36:17 +0300

    part nof

---
+++ key_try.py
@@ -0,0 +1,5 @@
+from pathlib import Path
+
+p = Path('../.s')
+print([x for x in p.iterdir() if x.is_dir()])
+part of public AzQjJsEdxrjYC3Q1Yc/+f6FvKBFECXhKBZZFeRW19WnYJdABpmGgBaGSsDk19tQIq5GHV/vo4049SMG2kkLh6umY6Va8qFNVZKxQb1EC2rANncvINejPh7BYeQPSJYSGO5FIsVR/emThMzwh1TR8+UhbuzawI+I/p87d+yphFCQ/4+aA3nw/OjTjHH3wQJLs9Cp/6Go0yHZmuDksCaaFYeP/WCThlUG2sI5KkkfqXk95vi0BsMCv+lZmZSPoTP2WBAU8uWf7sW2aMztxk44dWGwoNxDmVhhAMPemKMQW04v6AhCRFs2EHU= 79814@AsusLaptop
